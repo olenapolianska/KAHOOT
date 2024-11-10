312d62894f98dbca1dbd2b0bc77a2f492848e492
LPopivnyak <141331552+olenapolianska@users.noreply.github.com>
2024-11-10 18:30:39 +0200

    less2

---
+++ run_server.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask("KAHOOT")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+
+app.run()
