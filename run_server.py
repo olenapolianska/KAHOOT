517e78f33e893c98d23de02e9c48b88ebb1085a8
LPopivnyak <141331552+olenapolianska@users.noreply.github.com>
2024-12-01 18:27:42 +0200

    less?

--- run_server.py
+++ run_server.py
@@ -28,12 +28,23 @@ def show_question(quiz_id):
     db_manager = DBManager(db_name)
     options = db_manager.get_options(q[0])
 
-    return str(q) + "<br>" + str(options)
-
+    return render_template("question.html", question=q, options=options, quiz_id=quiz_id)
 
 @app.route("/about_school")
 def about_school():
     return render_template("about_school.html")
 
+
+@app.route("/quiz/<int:quiz_id>/answer",methods=["POST"])
+def answer_func(quiz_id):
+    session["quest_index"] += 1
+    if len(session["questions"]) <= session["quest_index"]:
+        return redirect(url_for("result", quiz_id=quiz_id))
+    else:
+        return redirect(url_for("show_question", quiz_id=quiz_id))
+
+@app.route("/quiz/<int:quiz_id>/result")
+def result(quiz_id):
+    return "РЕЗУЛЬТАТ"
 
 app.run()
